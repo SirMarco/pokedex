91d0ce12984cafa65d57c5b78ec22d365a7f7683
Marco Engelhardt <marco.engelhardt@reussnet.ch>
2023-11-10 12:59:24 +0100

    fixes templates

---
+++ js/template.js
@@ -0,0 +1,17 @@
+function generateCardsHTML(i, element) {
+    return /*html*/`
+    <div id="card${[i + 1]}" class="pokemonCard ${element['types'][0]['type']['name']}" onclick="openCard(${[i]})">
+      <div class="pokemonCardTop">
+        <h1 class="mt-2 text-white">${upperCase(element['name'])}</h1>
+        <p class="mt-2 text-white">${modifyId((element['id']))}</p>
+      </div>
+    <div class="pokemonCardBottom">
+      <div class="pokemonCardBottom-1">
+        <p class="${element['types'][0]['type']['name']}-text">${upperCase(element['types']['0']['type']['name'])}</p>
+      </div>
+      <div class="pokemonCardBottom-2">
+        <img src='${element['sprites']['other']['dream_world']['front_default']}'>
+      </div>
+    </div>
+    </div>`;
+}
